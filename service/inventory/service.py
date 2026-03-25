b2dfea55a42571aa367029095c8fd5b19f436342
joinercantillo <jcantill55@gmail.com>
2026-03-25 12:23:07 -0500

    modify service.py

--- service/inventory/service.py
+++ service/inventory/service.py
@@ -1,17 +1,19 @@
 def inventory_service():
-    while True:
-        inventory = [{potions: 0}, 
+    
+    inventory = [{potions: 0}, 
                      {heavyarmor: 0}, 
                      {lightarmor: 0}, 
                      {sword: 0},
                      {bow: 0},
                     {gold: 0}]
-        print('welcome to the inventory ' \
+    print('welcome to the inventory ' \
         '\n -1- use item \n -2- equip item ' \
         '\n -3- exit')
+    while True:
         option = int(input())
         if option == 1:
             print('what item do you want to use?')
             print(inventory)
+print(inventory_service())
 
         
