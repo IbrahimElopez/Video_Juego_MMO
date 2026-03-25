f14102e5610d000c53665c01659ea4159b5490e3
joinercantillo <jcantill55@gmail.com>
2026-03-25 11:40:30 -0500

    ss

--- service/inventory/service.py
+++ service/inventory/service.py
@@ -1,1 +1,7 @@
-print('sss')
+def inventory_service():
+    while True:
+        print('welcome to the inventory ' \
+        '\n -1- use item \n -2- equip item ' \
+        '\n -3- exit')
+        option = int(input())
+        
