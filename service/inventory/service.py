18b67f70e2ec88177bd191ddfba44eeb2a5f53fe
joinercantillo <jcantill55@gmail.com>
2026-03-25 12:52:13 -0500

    modify service.py

--- service/inventory/service.py
+++ service/inventory/service.py
@@ -1,14 +1,33 @@
 def inventory_service():
-    
-    inventory = {'potion': 0, 'sword': 0, 'shield': 0, 'heavyarmor': 0, 'lightarmor': 0, 'bow':0, 'gold': 0}
-    print(f'welcome to the inventory  {inventory}' 
-        '\n -1- use item \n -2- equip item ' \
-        '\n -3- exit')
-    while True:
+    while True:    
+        health = 100
+        defense = 0
+        atk = 0
+        inventory = {'potion': 0, 'gold': 0}
+        armor = {'shield': 0, 'heavyarmor': 0, 'lightarmor': 0}
+        weapons = {'sword': 0, 'bow':0}
+        print(f'welcome to the inventory  {inventory}' 
+            '\n -1- use a potion ' \
+            '\n -2- equip item ' \
+            '\n -3- exit')
+        
         option = int(input())
         if option == 1:
-            print('what item do you want to use?')
-            print(inventory)
-
-
+            for i,j in inventory.items():
+                if i == 'potion' and j > 0:
+                    print('you used a potion')
+                    health += 30
+                    inventory['potion'] -= 1
+                    break
+        elif option == 2:       
+            print('do you wanna equip a weapon or armor?')
+            option2 = int(input('1- weapon \n 2- armor \n'))
+            if option2 == 1:
+                enumerate_weapons = list(enumerate(weapons.keys(), start=1))
+                option3 = int(input('select the weapon you want to equip \n'))
+                
+            
+        elif option == 3:
+            break
+        
         
